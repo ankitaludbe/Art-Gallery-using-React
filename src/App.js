6394a868823cacf6b2c19c84e6aa3445d7468eeb
onkar lad <onkarlad426@gmail.com>
2023-08-21 19:05:14 +0530

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import React from 'react';
+import Navbar from './Component/Navbar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+    
+
+        <Navbar/>
+      
+    </>
   );
 }
 
---
+++ src/Component/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { NavLink ,BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Navbar = () => {
+  return (
+    <>
+    <BrowserRouter>
+        <div id='header' >
+            <div className="container-fluid">
+                {/* <div className="container"> */}
+                <div className="row ">
+                    <div id="firstsec" className='col-md-4 link'>
+                        <NavLink to="/" className="me-3">Menu</NavLink>
+                        <NavLink to="/">English</NavLink>
+                    </div>
+
+                    <div id="secsec" className='col-md-4'>
+                        <h2>LOUVRE</h2>
+                    </div>
+
+                    <div id="thirdsec" className='col-md-4'>
+                        <NavLink to="/" className="me-3">Online Boutique</NavLink>
+                        <NavLink to="/">Tickets</NavLink>
+                    </div>
+                </div>
+            {/* first row end */}
+                <hr />
+                <div className="row">
+                    <div id="navsecrow">
+                    <NavLink to="/" className="me-5">VISIT</NavLink>
+                    <NavLink to="/" className="me-5">EXPLORE</NavLink>
+                    <NavLink to="/">WHAT'S ON</NavLink>
+                    </div>
+                {/* </div> */}
+                </div>
+            </div>
+            
+        </div>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default Navbar
